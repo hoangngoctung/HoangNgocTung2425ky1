c6c42c311a8b65bdcc1321669ffcc556dcd31509
hoangngoctung <hoangngoctung22@gmail.com>
2024-10-17 18:57:34 +0700

    CreateProject

---
+++ HoangNgocTung705/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace HoangNgocTung705.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
